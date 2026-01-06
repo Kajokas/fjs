60dde5e5624e42a7b0137c9fa982f641515ac9e6
Kajus <kajus.tam@gmail.com>
2026-01-06 13:35:55 +0200

    Page entry point is now added by json

--- src/opts/build.rs
+++ src/opts/build.rs
@@ -1,5 +1,7 @@
 use std::{fs::{self, File}, io::Write, path::Path};
 
+use serde_json::Value;
+
 use crate::{opts::tmp::copy_dir_all, utils};
 
 fn build_prj(path: String, build_extra_route: Option<String>) {
@@ -8,18 +10,30 @@ fn build_prj(path: String, build_extra_route: Option<String>) {
 
     let mut file_bytes: Vec<u8> = Vec::new(); 
 
-    let mut routes: Vec<(String, String)> = Vec::new();
-
-    let r = String::from("/");
-    let mut p = build_extra_route.unwrap_or(String::from(""));
-    p.push_str("root");
-    routes.push((r, p));
+    let routes: Vec<(String, String)> = get_routes(build_extra_route);
 
     file_bytes.extend_from_slice(&utils::get_fs_server_start_bytes());
     file_bytes.extend_from_slice(&utils::get_fs_page_route_byte(routes));
     file_bytes.extend_from_slice(&utils::get_fs_server_end_bytes("3000"));
 
     fs_server_file.write(&file_bytes).expect("Failed to write data to file");
+}
+
+fn get_routes(build_extra_route: Option<String>) -> Vec<(String, String)> {
+    let mut routes: Vec<(String, String)> = Vec::new();
+
+    let routes_json_data = fs::read("./routes.json").expect("Failed to read routes json");
+    let routes_map:Value = serde_json::from_slice(&routes_json_data).expect("Failed to get routes from json");
+    let routes_map = routes_map.as_object().expect("Json format invalid in routes.json");
+
+    for (route_uri, route_dir) in routes_map.iter() {
+        let r = String::from(route_uri);
+        let mut p = build_extra_route.clone().unwrap_or(String::from(""));
+        p.push_str(route_dir.as_str().expect("Route page path must point to a real folder"));
+        routes.push((r, p));
+    }
+
+    routes
 }
 
 pub fn build(build_type: Option<String>) {
